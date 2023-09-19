c1c40d822c2407c4ed5f85be713789f80cb6591f
programmerasif <mdasifkhan3757@gmail.com>
2023-09-19 10:58:11 +0600

    added

--- src/app/navebr/page.js
+++ src/app/navebr/page.js
@@ -15,7 +15,36 @@ const Navbr = () => {
                      <input className="placeholder:italic bg-[#211A75] placeholder:text-slate-400 block  w-[490px] rounded-full py-4 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm" placeholder="Search for anything..." type="text" name="search"/>
                 </label>
                 </div>
-                <div>Hi</div>
+                <div className='flex justify-center items-center gap-14'>
+                    <div className='text-[#6418C3] underline'>OTHER MENUE</div>
+                    <div className='flex justify-center items-center gap-7'>
+                        <div className='relative'>
+                            <Image alt='bell' src="/bell.png" width={30} height={30}></Image>
+                            <span className='absolute -top-3 -right-2 bg-[#5ECFFF] rounded-full px-2'>12</span>
+                        </div>
+                        <div className='relative'>
+                            <Image alt='bell' src="/Video_Lesson.png" width={30} height={30}></Image>
+                            <span className='absolute -top-3 -right-2 bg-[#5ECFFF] rounded-full px-2'>5</span>
+                        </div>
+                        <div className='relative'>
+                            <Image alt='bell' src="/checkbox.png" width={30} height={30}></Image>
+                            <span className='absolute -top-3 -right-2 bg-[#5ECFFF] rounded-full px-2'>2</span>
+                        </div>
+                        <div className='relative'>
+                            <Image alt='bell' src="/office.png" width={30} height={30}></Image>
+                            <span className='absolute -top-3 -right-2 bg-[#E328AF] rounded-full px-2 text-sm'>!</span>
+                        </div>
+                    </div>
+                    <div>
+                        
+                    <div className='flex justify-center items-center bg-[#211A75] px-6 py-3 rounded-full gap-3 font-bold'>
+                        <Image alt='flag' src="/united-states.png" height={25} width={25}></Image>
+                        <span>England</span>
+                        
+                    </div>
+                    </div>
+
+                </div>
                 <div>Bye</div>
             </div>
         </div>
