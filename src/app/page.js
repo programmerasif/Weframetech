50ba01bdc19d34be76077ce18b7b4bf044550659
programmerasif <mdasifkhan3757@gmail.com>
2023-09-19 23:26:38 +0600

    everything is done

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,6 @@
 "use client"
 import { useState } from "react";
 import Dashboard from "./dashboard/page";
-import Image from "next/image";
 import { FaChevronLeft, FaChevronRight } from "react-icons/fa";
 import Container from "./container/page";
 
